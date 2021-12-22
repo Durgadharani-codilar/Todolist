8637e8244600b5295633b4b2055533563d98b023
Durgadharani-codilar <94517044+Durgadharani-codilar@users.noreply.github.com>
2021-12-22 10:20:55 +0530

    Update Todolist.js

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Todolist.css';
+
 const getLocalItems = () => {
     let lists = localStorage.getItem('list');
     if (lists) {
@@ -11,25 +12,26 @@ const getLocalItems = () => {
 function Todolist() {
     const [inputList, setinputList] = useState();
     const [items, setItems] = useState(getLocalItems());
-    const [status,setStatus]=useState('All');
-    const [filterTodos,setFilterTodos]=useState([]);
+    const [status, setStatus] = useState('All');
+    const [filterTodos, setFilterTodos] = useState([]);
     console.log(items);
     document.body.style.background = "black";
     const theme = () => {
-        var image = document.getElementById('change1');
+        var image = document.getElementById('theme1');
         if (image.src.match("images/icon-sun.svg")) {
             image.src = "images/icon-moon.svg";
         }
         else {
             image.src = "images/icon-sun.svg";
         }
-        var imagee = document.getElementById('change2');
-        if (imagee.src.match("images/bg-desktop-dark.jpg")) {
-            imagee.src = "images/bg-desktop-light.jpg";
+        var image1 = document.getElementById('theme2');
+        if (image1.src.match("images/bg-desktop-dark.jpg")) {
+            image1.src = "images/bg-desktop-light.jpg";
         }
         else {
-            imagee.src = "images/bg-desktop-dark.jpg";
+            image1.src = "images/bg-desktop-dark.jpg";
         }
+
         if (document.body.style.background === "black") {
             document.body.style.background = "white";
         }
@@ -41,75 +43,77 @@ function Todolist() {
         setinputList(event.target.value);
     }
     const addList = () => {
-        setItems([...items,{text:inputList,completed : false ,id:new Date().getTime()}]);
+        setItems([...items, { text: inputList, completed: false, id: new Date().getTime() }]);
     }
     useEffect(() => {
         localStorage.setItem('list', JSON.stringify(items))
         filterHandler();
-    }, [items,status])
+    }, [items, status])
     const checkBox = (ind) => {
-      const newId =items.map((el) => {
-          if(el.id === ind){
-              return {...el, completed :!el.completed};
-          }
-          else{
-                return el;
-          }
-      })
-      setItems(newId);
+        const newId = items.map((element) => {
+            if (element.id === ind) {
+                return { ...element, completed: !element.completed };
+            }
+            else {
+                return element;
+            }
+        })
+        setItems(newId);
     }
-    const deletee = (id) => {
-        const updatedItems=items.filter((elem,ind)=>{
+    const deletes = (id) => {
+        const updatedItems = items.filter((elem, ind) => {
             return ind !== id;
         });
-            console.log('hi');
-            console.log(id);
-            setItems(updatedItems);
+        console.log('hai');
+        console.log(id);
+        setItems(updatedItems);
+
     }
-    const setHandeler = (e) =>{
+    const setHandeler = (e) => {
         setStatus(e.target.textContent);
     }
-    const filterHandler = () =>{
-        switch(status){
-            case 'Completed' :
-                setFilterTodos(items.filter(el => el.completed === true));
+
+    const filterHandler = () => {
+        switch (status) {
+            case 'Completed':
+                setFilterTodos(items.filter(element => element.completed === true));
                 break;
-            case 'Active' :
-                setFilterTodos(items.filter(el => el.completed === false));
+            case 'Active':
+                setFilterTodos(items.filter(element => element.completed === false));
                 break;
             default:
                 setFilterTodos(items);
-    }
+        }
     }
     return (
         <>
             <div className='image'>
-                <img src="images/bg-background-dark.jpg" id="change2" alt="" />
+                <img src="images/bg-desktop-dark.jpg" id="theme2" alt="" />
             </div>
             <div className="parent">
                 <h1>
                     TODO <span id='light' onClick={theme}
-                    ><img src="images/icon-sun.svg" id="change1" alt="" />
+                    ><img src="images/icon-sun.svg" id="theme1" alt="" />
                     </span>
                 </h1>
+                <form>
                 <div className="input">
-                    <input type="text" placeh a new placeholder='Create a new todo...' onChange={itemEvents}  id='noo'/>
+                    <input type="text" placeholder='Enter a new todo...' onChange={itemEvents} id='noo' />
                     <button onClick={addList}>+</button>
                 </div>
+                </form>
 
             </div>
             <div className="entry">
                 <ol className='ol'>
 
-                    {filterTodos.map((itemVal, ind) => {
+                    {filterTodos.map((itemValue, ind) => {
                         return <li key={ind} className='list-item'>
-                            <input  type="checkbox" className='check' value=""  id="check"  
-                            checked={itemVal.completed} onClick={() => checkBox(itemVal.id)} /> 
-                           
-                          
-                            <span style={itemVal.completed?{textDecoration:"line-through"}:null}>{itemVal.text}</span>
-                            <img src="images/icon-cross.svg" onClick={()=>deletee(ind)} alt="" />
-                        </li> 
+                            <input type="checkbox" className='check' value="" id="check"
+                                checked={itemValue.completed} onClick={() => checkBox(itemValue.id)} />
+                            <span style={itemValue.completed ? { textDecoration: "line-through" } : null}>{itemValue.text}</span>
+                            <img src="images/icon-cross.svg" onClick={() => deletes(ind)} alt="" />
+                        </li>
                     })}
                 </ol>
 
